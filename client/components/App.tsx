fbe47fe0a5d869f06e7b6734a379568ce3c63f51
Sonia Huynh <soniahuynhkl@gmail.com>
2024-05-06 13:36:35 +1200

    update home page

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from './Navbar'
+import Home from './Home'
 function App() {
   return (
     <>
       <Navbar />
+      <Home />
     </>
   )
 }
