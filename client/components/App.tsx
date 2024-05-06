072bc7868a32a07e92479110c1afc85a6d279562
Sonia Huynh <soniahuynhkl@gmail.com>
2024-05-06 13:18:50 +1200

    test home page

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,9 +1,10 @@
+import { Outlet } from 'react-router-dom'
+import Navbar from './Navbar'
 function App() {
   return (
-    <div>
-      <h1>App</h1>
-      <p>React development has begun!</p>
-    </div>
+    <>
+      <Navbar />
+    </>
   )
 }
 
---
+++ client/components/Navbar.tsx
@@ -0,0 +1,39 @@
+export default function Navbar() {
+  return (
+    <>
+      <div>
+        <ul className="navbar">
+          <li>Home</li>
+          <li>My Projects</li>
+          <li>Blog Posts</li>
+          <li>Games</li>
+        </ul>
+      </div>
+      <div>
+        <img
+          id="helloimg"
+          className="center"
+          src="/public/images/hello-world.png"
+          alt="waving"
+        />
+      </div>
+      <div className="homediv">
+        <p>Hello there! </p>
+        <p>
+          My name is Sonia and I am an enthusiastic and compassionate full-stack
+          developer with an interest in creating projects that add entertainment
+          or improve our day-to-day lives. Take a look at the projects I&apos;ve
+          worked on and what I am currently working on in My Projects tab.
+          <p>
+            I enjoy the collaborative and problem solving aspect of development
+            and am always open to learning new concepts.
+          </p>
+        </p>
+        <p>
+          Outside of coding, I have an interest in reading, crocheting and
+          playing card/board games with my friends and family.
+        </p>
+      </div>
+    </>
+  )
+}
