5c7a19d37897fbf2645b010d38e13311150ec08e
Sonia Huynh <soniahuynhkl@gmail.com>
2024-06-26 15:12:04 +1200

    create menu for blog posts

---
+++ client/components/BookBoundBlog/Menu.tsx
@@ -0,0 +1,32 @@
+export default function Menu() {
+  return (
+    <>
+      <div className="homediv">
+        <h1>BookBound Blog Posts</h1>
+        <div className="homediv">
+          <ol>
+            <p>
+              <li>
+                <a href="/my-projects/BookBoundBlog/BookBound">Search Bar</a>
+              </li>
+            </p>
+            <p>
+              <li>
+                <a href="/my-projects/BookBoundBlog/localStorage">
+                  Save Book in Browser Local Storage
+                </a>
+              </li>
+            </p>
+            <p>
+              <li>
+                <a href="/my-projects/BookBoundBlog/starRating">
+                  Half Star Rating
+                </a>
+              </li>
+            </p>
+          </ol>
+        </div>
+      </div>
+    </>
+  )
+}
