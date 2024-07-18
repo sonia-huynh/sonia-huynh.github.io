05899baf8c81a233328ba00b3a08133f7ca2e435
Sonia Huynh <soniahuynhkl@gmail.com>
2024-07-18 15:21:45 +1200

    add simple qr project

--- client/components/Portfolio.tsx
+++ client/components/Portfolio.tsx
@@ -6,6 +6,7 @@ import about from '../../Public/images/about.png'
 import arrow from '../../Public/images/arrow.png'
 import check from '../../Public/images/checkmark.png'
 import email from '../../Public/images/email.png'
+import qr from '../../Public/assets/simple-qr.gif'
 import bookBound from '../../Public/assets/BookBoundDemo.mp4'
 import bean from '../../Public/assets/dreambeanmemeteam.mp4'
 import Lucas from '../../Public/assets/LucasMansionDemo.mp4'
@@ -24,11 +25,13 @@ function Portfolio() {
   const projectRef1 = useRef(null)
   const projectRef2 = useRef(null)
   const projectRef3 = useRef(null)
+  const projectRef4 = useRef(null)
   const isAboutInView = useInView(aboutRef, { once: true })
   const isTechInView = useInView(techRef, { once: true })
   const isProject1InView = useInView(projectRef1, { once: true })
   const isProject2InView = useInView(projectRef2, { once: true })
   const isProject3InView = useInView(projectRef3, { once: true })
+  const isProject4InView = useInView(projectRef4, { once: true })
 
   function toggleMenu() {
     setToggle((prevState) => !prevState)
@@ -38,6 +41,7 @@ function Portfolio() {
     window.open(CV, '_blank')
   }
 
+  // Framer Motion Animation
   const profileVariants = {
     hidden: {
       y: -200,
@@ -432,13 +436,114 @@ function Portfolio() {
               className="project-box"
               whileHover={{
                 scale: 1.05,
-                boxShadow: '0px 0px 8px rgb(210,105,30)',
+                boxShadow: '0px 0px 8px rgb(100,149,237)',
               }}
               variants={projectOddVariants}
               initial="hidden"
               animate={isProject1InView ? 'visible' : ''}
             >
-              <h2 className="project-title">BookBound</h2>
+              <h2 className="project-title">Simple QR Code</h2>
+              <div className="project-container">
+                <div className="gif-container">
+                  <img
+                    className="project-gif"
+                    src={qr}
+                    alt="sonia's qr code demo"
+                  />
+                </div>
+                <div className="text-container">
+                  <p>
+                    To practice my frontend CSS styling I completed the{' '}
+                    <a
+                      href="https://www.frontendmentor.io/challenges/qr-code-component-iux_sIO_H"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="other-links"
+                    >
+                      QR Code Component Challenge{' '}
+                    </a>
+                    from Frontend Mentor.
+                  </p>
+                  <br />
+                  <p>
+                    I tweaked the challenge a bit by adding a button, creating
+                    my own QR code that links to my github page and added some
+                    simple animation.
+                  </p>
+                  <br />
+                  <p>
+                    I then practiced my testing with React Testing Library by
+                    writing tests to test the Title exists, the button works and
+                    the button click shows the QR code.
+                  </p>
+                  <br />
+                  <p>
+                    I have also deployed this app on Vercel, click the deployed
+                    button to check it out.
+                  </p>
+                  <br />
+                  <h3>Tech Stack:</h3>
+                  <div className="project-badges-container">
+                    <img
+                      src="https://img.shields.io/badge/React-badge?style=flat&logo=react&color=07405E"
+                      alt="React"
+                      className="tech-badge"
+                    />
+                    <img
+                      src="https://img.shields.io/badge/JavaScript-07405E?style=flat&logo=javascript"
+                      alt="JavaScript"
+                      className="tech-badge"
+                    />
+                    <img
+                      src="https://img.shields.io/badge/HTML-07405E?style=flat&logo=html5"
+                      alt="HTML"
+                      className="tech-badge"
+                    />
+                    <img
+                      src="https://img.shields.io/badge/CSS-07405E?&style=flat&logo=css3"
+                      alt="CSS"
+                      className="tech-badge"
+                    />
+                    <img
+                      src="https://img.shields.io/badge/React Testing Library-07405E?&style=flat&logo=testinglibrary"
+                      alt="React Testing Library"
+                      className="tech-badge"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="project-button home-button-container">
+                <a
+                  href="https://github.com/sonia-huynh/simple-qr"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="home-button project-button">Github</button>
+                </a>
+                <a
+                  href="https://sonia-github-qr-code.vercel.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="home-button project-button">
+                    Deployed App
+                  </button>
+                </a>
+              </div>
+            </motion.div>
+            <motion.div
+              className="project-box"
+              whileHover={{
+                scale: 1.05,
+                boxShadow: '0px 0px 8px rgb(210,105,30)',
+              }}
+              variants={projectEvenVariants}
+              initial="hidden"
+              animate={isProject2InView ? 'visible' : ''}
+            >
+              <h2 className="project-title" ref={projectRef2}>
+                BookBound
+              </h2>
               <div className="project-container">
                 <video controls className="project-video">
                   <source src={bookBound} type="video/mp4" />
@@ -527,11 +632,11 @@ function Portfolio() {
                 scale: 1.05,
                 boxShadow: '0px 0px 8px 	rgb(139,0,0)',
               }}
-              variants={projectEvenVariants}
+              variants={projectOddVariants}
               initial="hidden"
-              animate={isProject2InView ? 'visible' : ''}
+              animate={isProject3InView ? 'visible' : ''}
             >
-              <h2 className="project-title" ref={projectRef2}>
+              <h2 className="project-title" ref={projectRef3}>
                 Lucas&apos; Mansion
               </h2>
               <div className="project-container">
@@ -626,11 +731,11 @@ function Portfolio() {
                 scale: 1.05,
                 boxShadow: '0px 0px 8px rgb(127,255,212)',
               }}
-              variants={projectOddVariants}
+              variants={projectEvenVariants}
               initial="hidden"
-              animate={isProject3InView ? 'visible' : ''}
+              animate={isProject4InView ? 'visible' : ''}
             >
-              <h2 className="project-title" ref={projectRef3}>
+              <h2 className="project-title" ref={projectRef4}>
                 DreamBeanMemeTeam
               </h2>
               <div className="project-container">
